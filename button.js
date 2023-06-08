8b773b482df8e25f70b68d33723fe39aeb68c788
Rifki Anashirul <68545605+rifkianashirul19@users.noreply.github.com>
2023-06-08 21:18:59 +0800

    Delete button.js

--- button.js
+++
@@ -1,8 +0,0 @@
-var myButton = document.getElementById('Tombol-mati');
-
-function disableButton() {
-    myButton.disabled = true;
-    myButton.textContent = "Telah Di Pesan";
-}
-
-myButton.addEventListener('Tekan', disableButton);
